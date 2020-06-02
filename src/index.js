2bd432422130c08fc814071c39c576bf27d97d9b
tt <truthtaicom>
2020-06-02 21:05:29 +0700

    adding route

--- src/index.js
+++ src/index.js
@@ -7,12 +7,37 @@ import Login from './pages/Login'
 import ProductDetail from './pages/ProductDetail';
 import * as serviceWorker from './serviceWorker';
 import { ThemeContextCustom } from './hooks/useBgMode'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeContextCustom>
-      <ProductDetail />
-    </ThemeContextCustom>
+    <Router>
+      <ThemeContextCustom>
+        <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
+
+          <Route exact path="/login">
+            <Login />
+          </Route>
+
+          <Route exact path="/register">
+            <Register />
+          </Route>
+
+          <Route exact path="/product-detail">
+            <ProductDetail />
+          </Route>
+
+          <Route path="*">
+            <h1>404</h1>
+          </Route>
+
+        </Switch>
+        
+      </ThemeContextCustom>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
